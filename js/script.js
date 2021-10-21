9d703782c25f279fb52505ceb4d18452efb7a4b9
Eduardo Lopes <65118593+duardoLopes@users.noreply.github.com>
2021-10-21 20:46:18 -0300

    upload new script

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let whatsapp = document.querySelector('#whatsapp');
+let reddit = document.querySelector('#reddit');
+let discord = document.querySelector('#discord');
+let twitter = document.querySelector('#twitter');
+let instagram = document.querySelector('#instagram');
+let telegram = document.querySelector('#telegram');
+
+whatsapp.addEventListener('click', function() {
+    window.open("https://chat.whatsapp.com/HX1bzh9PSuaCD9FWVnklyY", "_blank");
+});
+reddit.addEventListener('click', function() {
+    window.open("https://www.reddit.com/u/compilacao?utm_medium=android_app&utm_source=share", "_blank");
+});
+discord.addEventListener('click', function() {
+    window.open("https://discord.gg/az4tSXE", "_blank");
+});
+twitter.addEventListener('click', function() {
+    window.open("https://twitter.com/compilacaomr?s=09", "_blank");
+});
+instagram.addEventListener('click', function() {
+    window.open("https://www.instagram.com/compilacaodememesruins", "_blank");
+});
+telegram.addEventListener('click', function() {
+    window.open("https://t.me/compilacaodememesruins", "_blank");
+});
