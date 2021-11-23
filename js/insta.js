fa56faa40182ae579dfaff75852ef0e884edb611
Eduardo Lopes <eduardolopes42k@gmail.com>
2021-11-22 22:50:38 -0300

    mais ajustes

--- js/insta.js
+++ js/insta.js
@@ -2,7 +2,7 @@ let conta1 = document.querySelector('.conta1');
 let conta2 = document.querySelector('.conta2');
 
 conta1.addEventListener('click', function() {
-    alert('A conta foi pro beleleu');
+    alert('A conta foi pro beleleu, talvez ela volte outro dia!');
     window.open("https://www.instagram.com/compilacaodememesruins", "_blank");
 });
 conta2.addEventListener('click', function() {
